bfb5e057584bdc10d24fa8978663bb8dbe186474
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-08-11 18:49:01 +0530

    devenv setup; users and sessions table migration added

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+  env.DATABASE_URL = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git pkgs.postgresql_13 pkgs.dbmate];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  languages.rust.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
