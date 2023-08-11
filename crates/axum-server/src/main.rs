6711f8a6c2539d68ec702f05c940bef4eda63a80
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-08-11 20:42:31 +0530

    basic server now works
    to start from server side components

--- crates/axum-server/src/main.rs
+++ crates/axum-server/src/main.rs
@@ -1,5 +1,34 @@
 mod config;
+mod errors;
 
-fn main() {
-    println!("Hello, world!");
+use crate::errors::CustomError;
+use axum::{extract::Extension, response::Json, routing::get, Router};
+use db::User;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let config = config::Config::new();
+
+    let pool = db::create_pool(&config.database_url);
+
+    let app = Router::new()
+        .route("/", get(users))
+        .layer(Extension(config))
+        .layer(Extension(pool.clone()));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn users(Extension(pool): Extension<db::Pool>) -> Result<Json<Vec<User>>, CustomError> {
+    let client = pool.get().await?;
+
+    let users = db::queries::users::get_users().bind(&client).all().await?;
+
+    Ok(Json(users))
 }
--- devenv.nix
+++ devenv.nix
@@ -6,15 +6,17 @@
   env.DATABASE_URL = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.postgresql_13 pkgs.dbmate];
+  packages = [ pkgs.git pkgs.postgresql_13 pkgs.dbmate pkgs.cargo-watch pkgs.mold];
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo hello from $GREET";
+  scripts.cw.exec = "cargo watch -x run";
 
   enterShell = ''
     hello
     git --version
     export PATH=$HOME/.cargo/bin:$PATH
+    alias mold=${pkgs.mold}
   '';
 
   # https://devenv.sh/languages/
